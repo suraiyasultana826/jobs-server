d1cc1f98323c6d90a4ec725dccc1d47306c8f27a
suraiyasultana826 <suraiyasultana826@gmail.com>
2023-11-08 04:22:43 +0600

    delete job

--- index.js
+++ index.js
@@ -53,6 +53,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/postedJob/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await postedJobCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     app.get('/webdev/:id', async(req, res) => {
       const id = req.params.id;
